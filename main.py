a9f38ffcf35acba7eae5279d510396344f9e4b80
Jelmar Gerritsen <jelmargerritsen@gmail.com>
2020-03-03 17:25:38 +0100

    Add dendrograms

--- main.py
+++ main.py
@@ -69,67 +69,88 @@ model.compile(optimizer=keras.optimizers.RMSprop(learning_rate=LEARNING_RATE,),
 # Custom callbacks for viewing the model in TensorBoard; which is launched with command:
 # $ tensorboard --logdir logs/fit
 log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+file_writer = tf.summary.create_file_writer(log_dir)
+file_writer.set_as_default()
 
-def data_to_image(rep_df, epoch):
-  """Plots the provided representation, then converts it to a PNG image and
-  returns it. The created figure is closed and inaccessible after this call."""
-  figure = plt.figure(figsize=(8, 8))
-  cmap = sns.diverging_palette(220, 10, as_cmap=True)
-  image = sns.heatmap(rep_df, cmap=cmap, square=True)
-  plt.title('Distance matrix epoch {}'.format(epoch))
-  plt.tight_layout()
-  # Save the plot to a PNG in memory.
-  buf = io.BytesIO()
-  plt.savefig(buf, format='png')
-  # Closing the figure prevents it from being displayed directly inside
-  # the notebook.
-  plt.close(figure)
-  buf.seek(0)
-  # Convert PNG buffer to TF image
-  image = tf.image.decode_png(buf.getvalue(), channels=4)
-  # Add the batch dimension
-  image = tf.expand_dims(image, 0)
-  return image
+
+def plot_to_image(figure):
+    """Plots the provided representation, then converts it to a PNG image and
+    returns it as a tensor. The created figure is closed and inaccessible after this call."""
+    # Save the plot to a PNG in memory.
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png')
+    # Closing the figure prevents it from being displayed directly inside
+    # the notebook.
+    plt.close(figure)
+    buf.seek(0)
+    # Convert PNG buffer to TF image
+    image = tf.image.decode_png(buf.getvalue(), channels=4)
+    # Add the batch dimension
+    image = tf.expand_dims(image, 0)
+    return image
+
 
 class CustomLogger(keras.callbacks.Callback):
     """
     Custom callback which runs during the model training session and logs custom metrics
     """
-    def __init__(self, tensorboard_callback):
-        self.tensorboard_callback = tensorboard_callback
-        self.reps = []
 
-    def on_train_begin(self, logs=None):
-        writer = self.tensorboard_callback._get_writer(
-            self.tensorboard_callback._train_run_name)
-        self.writer = writer
+    def __init__(self):
+        self.distance_matrices = []
+        self.dendrograms = []
+
+    def add_image_summaries(self, epoch):
+        [weights, biases] = model.get_layer(
+            name='representations').get_weights()
+        current_representation = tf.squeeze(weights).numpy()
+        df = pd.DataFrame(current_representation, columns=[
+            i for i in range(len(current_representation))], index=items)
+        rep_df = pd.DataFrame(sp.spatial.distance_matrix(
+            df.values, df.values), index=df.index, columns=df.index)
+
+        # Add distance matrix
+        distance_figure = plt.figure(figsize=(8, 8))
+        plt.title('Distance matrix epoch {}'.format(epoch))
+        plt.tight_layout()
+        cmap = sns.diverging_palette(220, 10, as_cmap=True)
+        sns.heatmap(rep_df, cmap=cmap, square=True)
+        distance_matrix = plot_to_image(distance_figure)
+        self.distance_matrices.append(distance_matrix)
+
+        # Add dendrogram
+        dendrogram_figure = plt.figure(figsize=(8, 8))
+        plt.title('Dendrogram epoch {}'.format(epoch))
+        plt.tight_layout()
+        linkage = sp.cluster.hierarchy.linkage(
+            sp.spatial.distance.squareform(rep_df.values))
+        sp.cluster.hierarchy.dendrogram(
+            linkage, labels=items, leaf_rotation=90., show_contracted=True)
+        dendrogram = plot_to_image(dendrogram_figure)
+        self.dendrograms.append(dendrogram)
 
     def on_epoch_end(self, epoch, logs=None):
-        # On the last epoch, this isn't run - weirdly - so we need the second condition to log a summary
-        if epoch % 500 == 0 or epoch == NUM_EPOCHS - 1:
-            [weights, biases] = model.get_layer(
-                name='representations').get_weights()
-            current_representation = tf.squeeze(weights)
-            df = pd.DataFrame(current_representation.numpy(), columns=[
-                              i for i in range(len(current_representation))], index=items)
-            rep_df = pd.DataFrame(sp.spatial.distance_matrix(
-                df.values, df.values), index=df.index, columns=df.index)
+        if epoch % 500 == 0:
+            self.add_image_summaries(epoch)
 
-            # Plot it
-            final_image = data_to_image(rep_df, epoch)
+    def on_train_end(self, logs=None):
+        # On the last epoch, this isn't run - weirdly - so we need to add the last image here
+        self.add_image_summaries(NUM_EPOCHS)
 
-            # And write it
-            with self.writer.as_default():
-                summary = tf.summary.image("Distance Matrix", final_image, step=epoch)
-                self.writer.flush()
+        # Create image summaries all at once so they are show side-by-side (so step 0 is false; look at plot titles instead)
+        with file_writer.as_default():
+            distance_matrices = tf.stack(
+                tf.squeeze(self.distance_matrices), axis=0)
+            dendrograms = tf.stack(tf.squeeze(self.dendrograms), axis=0)
+            tf.summary.image(
+                "Distance Matrix", distance_matrices, step=0, max_outputs=len(distance_matrices))
+            tf.summary.image("Dendrogram", dendrograms, step=0,
+                            max_outputs=len(dendrograms))
 
-        def on_train_end(self, logs=None):
-            self.writer.close()
 
 tensorboard_callback = keras.callbacks.TensorBoard(
     log_dir=log_dir, histogram_freq=1, embeddings_freq=50)
 
-my_callback = CustomLogger(tensorboard_callback)
+my_callback = CustomLogger()
 
 history = model.fit(input_tensor,
-                    output_tensor, epochs=NUM_EPOCHS, callbacks=[tensorboard_callback, my_callback])
+                    output_tensor, epochs=NUM_EPOCHS, verbose=0, callbacks=[tensorboard_callback, my_callback])
